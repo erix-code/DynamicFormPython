628d72f8a39bb4e870b0430da307befb239006d5
x-lil-peep-x <ermateze@gmail.com>
2020-09-05 18:38:34 -0400

    Starting the project and adding the first Classes: Form, Question, QuestionType

---
+++ Form.py
@@ -0,0 +1,14 @@
+from Question import *
+
+
+class Form:
+
+    def __init__(self, name):
+        self.questions = []
+        self.name = name
+
+d = Form('Shit')
+d1 = Form('Shit')
+d2 = Form('Shit')
+d3 = Form('Shit')
+print(Form.counter)
---
+++ QuestionType.py
@@ -0,0 +1,14 @@
+class QuestionType:
+    def __init__(self, name):
+        self.name = name
+
+
+question_types = [
+    QuestionType('TEXT'),
+    QuestionType('NUMBER'),
+    QuestionType('DROPDOWN'),
+    QuestionType('DATE'),
+]
+
+
+
